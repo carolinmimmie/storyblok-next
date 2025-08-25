a42284c8de65c3ee5f5a0fdbe03e42a426daed8b
carolinmimmie <carolinmimmie@gmail.com>
2025-08-25 12:37:36 +0200

    any rule

--- src/components/RecommendedTour.tsx
+++ src/components/RecommendedTour.tsx
@@ -1,27 +1,41 @@
 import Link from "next/link";
 import React from "react";
+import type { ISbStoryData, ISbComponentType } from "@storyblok/react";
 
-const RecommendedTour = (props: any) => {
+interface IRecommendedTourContent {
+  name: string;
+  price: number;
+  location: string;
+  main_image: {
+    filename: string;
+    alt: string;
+  };
+}
+
+interface RecommendedTourProps {
+  story: ISbStoryData<ISbComponentType<string> & IRecommendedTourContent>;
+}
+
+const RecommendedTour: React.FC<RecommendedTourProps> = ({ story }) => {
   return (
-    <div className="bh-white rounded-sm shadow">
+    <div className="bg-white rounded-sm shadow">
       <img
         className="aspect-video object-cover w-full"
-        src={props.story.content.main_image.filename}
-        alt={props.story.content.main_image.alt}
+        src={story.content.main_image.filename}
+        alt={story.content.main_image.alt}
       />
       <div className="p-8">
         <div className="flex gap-4 justify-between text-lg font-bold">
-          <h3>{props.story.content.name}</h3>
-          <p>${Number(props.story.content.price).toFixed(2)}</p>
+          <h3>{story.content.name}</h3>
+          <p>${Number(story.content.price).toFixed(2)}</p>
         </div>
         <p className="text-gray-700 uppercase font-bold mt-2 text-sm tracking-wide">
-          {props.story.content.location}, Spain
+          {story.content.location}, Spain
         </p>
         <Link
           className="font-bold text-base mt-8 block underline"
-          href={`/${props.story.full_slug}`}
+          href={`/${story.full_slug}`}
         >
-          {" "}
           View tour
         </Link>
       </div>
